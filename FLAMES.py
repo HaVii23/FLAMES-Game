e5a8c063446bfd0e6d09ac9e8614a13cd725b57c
Hari Vignesh A <harivignesh201@gmail.com>
2022-08-08 22:54:28 +0530

    Add Name for Alphabetical Check

--- FLAMES.py
+++ FLAMES.py
@@ -1,16 +1,22 @@
+import sys
+
 def nameProcessing():
     name1 = input('Enter First Name  : ');    name1 = name1.upper().replace(' ','')
     name2 = input('Enter Second Name : ');    name2 = name2.upper().replace(' ','')
-    cut = 0
-    totNameLen = len(name1) + len (name2)
-    for a in name1:
-        for b in name2:
-            if (a==b):
-                name2=name2.replace(b,'',1)
-                cut +=1
-                break
-    flamesCount = totNameLen - (cut*2)
-    return flamesCount
+    if (name1.isalpha() ==True and name2.isalpha()== True):
+        cut = 0
+        totNameLen = len(name1) + len (name2)
+        for a in name1:
+            for b in name2:
+                if (a==b):
+                    name2=name2.replace(b,'',1)
+                    cut +=1
+                    break
+        flamesCount = totNameLen - (cut*2)
+        return flamesCount
+    else:
+        print("\nKindly Rename your Name with \"ONLY ALPHABETS\"...!!!\n\n")
+        sys.exit()
 
 def flameFinder():
     flames = ("FRIENDS", "LOVE", "AFFECTION", "MARRIAGE", "ENEMY", "SIBLINGS")
