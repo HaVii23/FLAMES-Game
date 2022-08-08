a5b4c6a020a138a0fed1e327558663a72ccbf1dc
Hari Vignesh A <harivignesh201@gmail.com>
2022-08-08 22:41:10 +0530

    Error correction 'Break' in nameProcessing Function

--- FLAMES.py
+++ FLAMES.py
@@ -1,19 +1,23 @@
 def nameProcessing():
-    name1 = input('Enter First Name  : ');    name1 = name1.upper();    name1.replace(" ", "")  
-    name2 = input('Enter Second Name : ');    name2 = name2.upper();    name2.replace(" ", "") 
+    name1 = input('Enter First Name  : ');    name1 = name1.upper().replace(' ','')
+    name2 = input('Enter Second Name : ');    name2 = name2.upper().replace(' ','')
     cut = 0
     totNameLen = len(name1) + len (name2)
+    print(name1,name2)
     for a in name1:
         for b in name2:
             if (a==b):
                 name2=name2.replace(b,'',1)
                 cut +=1
+                print(b)
+                break
     flamesCount = totNameLen - (cut*2)
+    print(flamesCount)
     return flamesCount
 
 def flameFinder():
     flames = ("FRIENDS", "LOVE", "AFFECTION", "MARRIAGE", "ENEMY", "SIBLINGS")
-    a = 2;    b = 0;    j = 1
+    a = 10;    b = 0;    j = 1
     flamesCutCount = nameProcessing()
     while(j<6):
         if(len(flames) != 1):
@@ -25,6 +29,7 @@ def flameFinder():
                     flameList.remove(i)
                     flames= tuple(flameList)
                     flames = flames[Ind:] + flames [:Ind]
+                    print(flames)
                     a += 1
             b = 0
         j+=1
